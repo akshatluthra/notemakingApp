2725f8e807099bbb70f79764319bbb21c5d0e8d8
akshatluthra <akshatluthra981@gmail.com>
2022-10-21 15:25:29 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPenToSquare, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'note-making-app';
+  edit = faPenToSquare
+  Close = faXmark
+  ChangeColor=true
+  notesArray:any = [];
+  addNotes(item:any){
+    if(item){
+      this.notesArray.push(item);
+    }
+    else{
+      alert("Please Enter something")
+    }
+  }
+  deleteNotes(id:number){
+    this.notesArray.splice(id, 1);
+  }
+  editNotes(){
+    console.log("Note Button is pressed")
+  }
 }
